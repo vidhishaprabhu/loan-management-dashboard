7dbbe473f12066b95a5aa0a116c9a4ca06be4cba
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-12-21 19:37:45 +0530

    Created the nav and dashboard component and provided the route for navbar element to navigate to required page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,MatToolbarModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
